d8f6042674e1d956080ac0611d849b4eec37f939
idmitryd <dolzhikov.d.a@gmail.com>
2021-12-11 19:32:31 +0300

    minor

--- functions/prj_setup.fish
+++ functions/prj_setup.fish
@@ -1,5 +1,14 @@
 function prj_setup
+    mkdir -p ./$argv/src
     mkdir ./$argv/include
-    mkdir ./$argv/src
-    mv ./$argv/main.cpp ./$argv/src/
+    mkdir ./$argv/build
+    cd $argv
+    set TemplateDir /home/dmitryd/coursera/templates/
+    cp $TemplateDir/CMakeLists.txt .
+    sed -i "4s/project_name/$argv/" CMakeLists.txt
+    cp $TemplateDir/main.cpp ./src/$argv.cpp
+    cd build
+    cmake .. > /dev/null
+    cp compile_commands.json ..
+    cd ..
 end
